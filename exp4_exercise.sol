8c2aa7e7e5ed2756db676570196f14b582a12365
andromeda0004 <akhilbj04@gmail.com>
2025-08-04 10:01:04 +0530

    ex

---
+++ exp4_exercise.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract VendingMachine {
+    address public owner;
+    mapping(address => uint256) public donutBalances;
+
+    constructor() {
+        owner = msg.sender;
+        donutBalances[address(this)] = 100;
+    }
+
+    function getVendingMachineBalance() public view returns (uint256) {
+        return donutBalances[address(this)];
+    }
+
+    function restock(uint256 amount) public {
+        require(msg.sender == owner, "Only the owner can restock.");
+        donutBalances[address(this)] += amount;
+    }
+
+    function purchase(uint256 amount) public payable {
+        require(
+            msg.value >= amount * 2 ether,
+            "You must pay at least 2 ETH per donut"
+        );
+        require(
+            donutBalances[address(this)] >= amount,
+            "Not enough donuts in stock to complete this purchase"
+        );
+        donutBalances[address(this)] -= amount;
+        donutBalances[msg.sender] += amount;
+    }
+}
